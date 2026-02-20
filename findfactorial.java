224593281be42fb07c35ad20af69f255a6471ad0
unknown <sona9946659656@gmail.com>
2026-02-20 13:02:23 +0530

    second commit

---
+++ findfactorial.java
@@ -0,0 +1,30 @@
+package Assignmentquest;
+
+import java.util.Scanner;
+
+public class findfactorial {
+
+	public static void main(String[] args) {
+		//finding factorials using loops
+//		int a=5;
+//		long fact=1;
+//		for(int i=1;i<=a;i++)
+//		{
+//			fact=fact*i;
+//			
+//		}
+//		System.out.println("factorial of 5 = "+fact);
+		Scanner sc=new Scanner(System.in);
+		String a=sc.nextLine();
+		int x=Integer.parseInt(a);
+		long f=1;
+		for(int i=1;i<=x;i++)
+		{
+			f=f*i;
+		}
+		System.out.println("factorial of "+a+" is "+f);
+		
+		
+	}
+
+}
